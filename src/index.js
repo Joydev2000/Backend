b5180a518aab1a96353a8f022d8fd69da7e13b54
Joydev2000 <137753258+Joydev2000@users.noreply.github.com>
2025-10-28 09:37:02 +0530

    user model

--- src/index.js
+++ src/index.js
@@ -1,52 +1,18 @@
 import dotenv from "dotenv";
-
-import  connectDB from "./db/index.js"
-
+// import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path: "./.env"
-
 });
 
-
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// (async () =>{
-//     try{
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error", (error) =>{
-//             console.log("Error:", error )
-//             throw error;
-//         })
-//         app.listen(process.env.PORT,() =>{
-//             console.log(`Server is running on port ${process.env.PORT}`)
-//         })
-//     }catch(error){
-//         console.log("Error:",error)
-//         throw error;
-        
-    
-//     }
-
-// })()
+connectDB()
+    // .then(() => {
+    //     app.listen(process.env.PORT || 8000, () => {
+    //         console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    //     });
+    // })
+    // .catch((error) => {
+    //     console.log("MongoDB connection failed:", error);
+    //     process.exit(1);
+    // });
